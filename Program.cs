793d1b136e64e5cf3be8a4f7b602c63a9065a603
Andrea Pullen <apullenb@gmail.com>
2021-04-08 11:36:16 -0500

    start game

--- Program.cs
+++ Program.cs
@@ -19,6 +19,39 @@ namespace NumberGuesser
             //Write app info
             Console.WriteLine("{0}: Version {1} by {2}", appName, appVersion, appAuthor);
 
+            Console.ResetColor();
+
+            //Ask for input
+            Console.WriteLine("What is Your Name?");
+
+            string inputName = Console.ReadLine();
+
+            Console.WriteLine("Hello {0}, let's play a game!", inputName);
+
+            // Init correct number
+            int correctNumber = 7;
+
+            int guess = 0;
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            while(guess != correctNumber)
+            {
+                string input = Console.ReadLine();
+                guess = Int32.Parse(input);
+
+                if(guess != correctNumber)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("{0} is not the correct number. Please try again.", input);
+                    Console.ResetColor();
+                    Console.WriteLine("Guess a number between 1 and 10");
+
+                }
+
+            }
+
+
+
 
         }
     }
